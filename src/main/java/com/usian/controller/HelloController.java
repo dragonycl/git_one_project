2b3e921d0f1f08cbfd9b6a1ea343ef72ff460ddb
龙忆 <7986175+Drogon-yi@user.noreply.gitee.com>
2020-10-29 11:18:29 +0800

    修改hellocontroller

--- src/main/java/com/usian/controller/HelloController.java
+++ src/main/java/com/usian/controller/HelloController.java
@@ -13,6 +13,7 @@ public class HelloController {
 
     @RequestMapping("test")
     public String h1(){
+        System.out.println("你好啊 学哥");
         logger.info("==============hello==================");
         return "index";
     }
